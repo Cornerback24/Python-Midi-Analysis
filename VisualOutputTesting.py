c121728b45d8e9a9578f5f2f36a14be8b3dc772f
Cornerback24 <6739979+Cornerback24@users.noreply.github.com>
2014-06-18 16:37:17 -0600

    Midi Parser detects end of file

--- VisualOutputTesting.py
+++ VisualOutputTesting.py
@@ -1,4 +1,9 @@
 from MidiParser import MidiParser
 
 midiParser = MidiParser("testMidiFile.mid")
+a = (midiParser.readNextData() + midiParser.readNextData()
+    + midiParser.readNextData() + midiParser.readNextData())
+print(str(a))
+while midiParser.hasMoreData():
+    print(midiParser.readNextData())
 midiParser.close()
