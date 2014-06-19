c7cd8540a5743c59ccb77392a239a23aab26b9bd
Nicholas Masching <nmaschin@mines.edu>
2014-06-18 18:00:51 -0600

    MidiParser parses Chuck ID with chunk size, as well as body of header chunk

--- VisualOutputTesting.py
+++ VisualOutputTesting.py
@@ -1,9 +1,17 @@
 from MidiParser import MidiParser
 
+
 midiParser = MidiParser("testMidiFile.mid")
+
+'''
 a = (midiParser.readNextData() + midiParser.readNextData()
     + midiParser.readNextData() + midiParser.readNextData())
 print(str(a))
 while midiParser.hasMoreData():
     print(midiParser.readNextData())
+'''
+print("Header def and size: " + str(midiParser.readNextData()))
+print("Body of header chunk: " + str(midiParser.readNextData()))
+print("Track def and size: " + str(midiParser.readNextData()))
+print(str(midiParser.readNextData()))
 midiParser.close()
