77c19934da70e98b76b124a2e2efc869b0b6a75f
Cornerback24 <6739979+Cornerback24@users.noreply.github.com>
2014-09-06 13:25:18 -0600

    Added MidiEventDecoder class

---
+++ MidiEventDecoder.py
@@ -0,0 +1,9 @@
+#decodes data from the MidiParser into data easier to work with
+class MidiEventDecoder:
+    def __init__():
+        return
+
+#contains all data about and event
+class MidiEvent:
+    def __init__(midiData):
+        return
