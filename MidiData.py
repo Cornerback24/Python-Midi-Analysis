2f74e11b85197dcfee41abcfbce7f9827eb54f5e
Nicholas Masching <nmaschin@mines.edu>
2015-01-31 17:47:06 -0700

    MidiData creates dictionary of TrackData

--- MidiData.py
+++ MidiData.py
@@ -1,4 +1,5 @@
 from MidiEventDecoder import MidiEventDecoder
+from TrackData import TrackData
 
 #contains the finalized data after anylisis
 class MidiData:
@@ -20,6 +21,7 @@ class MidiData:
         #maps running total of delta times to microsecondsPerQuarter
         self.tempoChanges = {}
         self.trackZeroEvents = []
+        self.tracks = {} #map instrument names to tracks
 
         deltaTimeTotal = 0    
         #should be a track header
@@ -36,12 +38,18 @@ class MidiData:
                 self.tempoChanges[deltaTimeTotal] = event.usPerQuarter
                 
 
+        #read in each track
+        tracknum = 0 #used to create temprary track names
         while self.eventDecoder.hasMoreEvents():
+            tracknum = tracknum + 1
+            trackName = "Track" + str(tracknum)
             #should be a track header
             event = self.eventDecoder.nextEvent()
+            trackData = TrackData()
             while not(event.eventClass == "Meta"
                 and event.eventType == "EndOfTrack"):
                 event = self.eventDecoder.nextEvent()
+            self.tracks[trackName] = trackData
             
 #this line just for testing
 MidiData("testMidiFile.mid")
--- TrackData.py
+++ TrackData.py
@@ -1,5 +1,7 @@
 class TrackData:
     def __init__(self):
+        self.notes = []
+        self.events = []
         return
     def addEvent(self, event):
         return
