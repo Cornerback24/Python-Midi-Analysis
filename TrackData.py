924dcdf5e7af328fab8b12f58a75657a32682fc3
Cornerback24 <6739979+Cornerback24@users.noreply.github.com>
2015-02-03 22:52:32 -0700

    Added TempoChanges class

--- TrackData.py
+++ TrackData.py
@@ -1,7 +1,35 @@
 class TrackData:
-    def __init__(self):
+    def __init__(self, name=""):
         self.notes = []
         self.events = []
+        self.name = name
         return
     def addEvent(self, event):
+        self.events.append(event)
+        #print(event)
+        if (event.eventClass == "Meta" and
+            event.eventType == "Sequence/TrackName"):
+            self.name = event.data
         return
+
+#this is basiclly and ordered dictionary
+class TempoChanges:
+    def __init__(self):
+        self.deltaTimeTotal = []
+        self.tempoChangeEvents = []
+        self.index = 0
+        return
+    #tempo changes need to be added in order
+    def addTempoChange(self, deltaTimeTotal, event):
+        self.deltaTimeTotal.append(deltaTimeTotal)
+        self.tempoChangeEvents.append(event)
+    #so that class can be used as a stream
+    def current(self):
+        return (self.deltaTimeTotal[self.index],
+                self.tempoChangeEvets[self.index])
+    def findNext(self):
+        self.index = self.index + 1
+    def hasMoreTempoChanges(self):
+        return self.index < len(self.tempoChangeEvents)
+    def reset(self): #go back to first task
+        self.index = 0
