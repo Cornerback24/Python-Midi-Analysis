089b573c92a4ea7c8260ee1973842de7685496d5
Cornerback24 <6739979+Cornerback24@users.noreply.github.com>
2016-10-22 22:01:30 -0600

    Note start and stop times generated for single-channel-per-track type 1 midi files

--- TrackData.py
+++ TrackData.py
@@ -1,49 +1,83 @@
 from Note import Note
+from MidiEvents import *
+import copy, bisect
 
 #contains data for a single track
 class TrackData:
     def __init__(self, name=""):
         self.notes = []
-        self.incompleteNotes = {} #maps pitches to notes without end times
+        #maps pitches to notes without end times
+        self.incompleteNotes = {}
         self.events = []
         self.name = name
+        self.deltaTimeTotal = 0
+        #if false, time division is frames per second
+        self.isTicksPerBeat = True
+        self.debug = False
         return
+    #Events need to be added in order, last event must be end of track
     def addEvent(self, event):
         self.events.append(event)
-        if (event.eventClass == "Meta" and
-            event.eventType == "Sequence/TrackName"):
-            self.name = event.text
-        if (event.eventClass == "Channel" and event.eventType == "NoteOn"):
-            self.incompleteNotes[event.noteNumber] = Note(event.startTime,
-                                                          event.noteNumber,
-                                                          event.velocity,
-                                                          event.channel)
-        if (event.eventClass == "Channel" and event.eventType == "NoteOff"):
-            self.incompleteNotes[event.noteNumber].setEndTime(event.startTime)
-            self.notes.append(self.incompleteNotes[event.noteNumber])
-            del self.incompleteNotes[event.noteNumber]
-        if (event.eventClass == "Meta" and event.eventType == "EndOfTrack"):
+        if (isinstance(event, TrackNameEvent)):
+            self.name = event.trackName
+        elif (isinstance(event, NoteOnEvent) and
+              not(event.isNoteOff())):
+            if event.noteNumber in self.incompleteNotes and self.debug:
+                print("Note on event for note " + str(event.noteNumber)
+                      + " already playing, skipping...")
+            else:
+                self.incompleteNotes[event.noteNumber] = Note(event.startTime,
+                                                              event.noteNumber,
+                                                              event.velocity,
+                                                              event.channel)
+        elif (isinstance(event, NoteOffEvent) or
+              (isinstance(event, NoteOnEvent) and event.isNoteOff())):
+            if event.noteNumber in self.incompleteNotes:
+                self.incompleteNotes[event.noteNumber].setEndTime(event.startTime)
+                self.notes.append(self.incompleteNotes[event.noteNumber])
+                del self.incompleteNotes[event.noteNumber]
+            elif self.debug:
+                print("Note off event for note " + str(event.noteNumber)
+                      + " not playing, skipping...")
+        elif (isinstance(event, EndOfTrackEvent)):
             self.notes.sort()
+    def setTempoChanges(self, tempoChanges):
+        self.tempoChanges = copy.deepcopy(tempoChanges)
+    def getTempoChanges(self, tempoChanges):
+        return copy.deepcopy(self.tempoChanges)
 
 #this is kind of like an ordered dictionary
 class TempoChanges:
     def __init__(self):
-        self.deltaTimeTotals = []
-        self.tempoChangeEvents = []
+        self.tempoChanges = []
         self.index = 0
         return
     #tempo changes need to be added in order
-    def addTempoChange(self, deltaTimeTotal, event):
-        self.deltaTimeTotals.append(deltaTimeTotal)
-        self.tempoChangeEvents.append(event)
+    #tempo in microseconds per quarter note
+    def addTempoChange(self, deltaTimeTotal, tempo):
+        #TODO change index?
+        bisect.insort(self.tempoChanges,
+                      TempoChange(deltaTimeTotal, tempo))
     #so that class can be used as a stream
     def deltaTimeTotal(self):
-        return self.deltaTimeTotals[self.index]
+        return self.tempoChanges[self.index].deltaTimeTotal
     def usPerQuarter(self):
-        return self.tempoChangeEvents[self.index].usPerQuarter
+        return self.tempoChanges[self.index].tempo
     def findNext(self):
         self.index = self.index + 1
+    #returns true if the current index is a tempo change
+    #(will return true if findNext will go beyond end of
+    # list, this is intentional)
     def hasMore(self):
-        return self.index + 1 < len(self.tempoChangeEvents)
+        return self.index < len(self.tempoChanges)
     def reset(self): #go back to first tempo change
         self.index = 0
+
+class TempoChange:
+    #deltaTimeTotal in ticks
+    #tempo in microseconds per quarter note
+    def __init__(self, deltaTimeTotal, tempo):
+        self.deltaTimeTotal = deltaTimeTotal
+        self.tempo = tempo
+    def __lt__(self, other):
+        return self.deltaTimeTotal < other.deltaTimeTotal
--- VisualOutputTesting.py
+++ VisualOutputTesting.py
@@ -2,11 +2,11 @@ from MidiParser import MidiParser
 from MidiData import MidiData
 from Util import Util
 from MidiEventDecoder import MidiEventDecoder
+from MidiEvents import *
 
-midi_file = "testMidiFile.mid" #testMidiFile.mid
+midi_file = "testMidiFile.mid"
+midi_file = "A Changeling Can Change_2.mid"
 print(midi_file)
-midiParser = MidiParser(midi_file)
-
 
 def printHex(_bytes):
     temp = ""
@@ -14,59 +14,60 @@ def printHex(_bytes):
         temp = temp + " " + str(hex(_bytes[i]))
     print(temp)
 
-'''
-print("-----------------raw file-----------------------")
-with open(midi_file, 'rb') as file:
-   printHex(file.read())
-file.close()
-print()
-print()
-'''
+def printRawFile():
+    print("-----------------raw file-----------------------")
+    with open(midi_file, 'rb') as file:
+        printHex(file.read())
+    file.close()
+    print()
+    print()
+    
+def testMidiParser():
+    midiParser = MidiParser(midi_file)
+    print("---------------Testing MidiParser--------------")
+    print("Header def and size: " + str(midiParser.readNextData()))
+    headerBody = midiParser.readNextData()
+    print("Body of header chunk: " + str(headerBody) + " number of tracks: " + 
+           str(int.from_bytes(headerBody[2:4], "big")))
+    while midiParser.hasMoreData():
+    #for i in range(2):
+        trackDef = midiParser.readNextData()
+        print("Track def and size: " + str(trackDef) + " track size: "
+            + str(int.from_bytes(trackDef[4:8], "big")))
+        while midiParser.bytesLeftInChunk > 0:
+            print(str(midiParser.readNextData()) + " size left: " +
+                str(midiParser.bytesLeftInChunk))
+        print()
+    midiParser.close()
 
-'''
-a = (midiParser.readNextData() + midiParser.readNextData()
-    + midiParser.readNextData() + midiParser.readNextData())
-print(str(a))
-while midiParser.hasMoreData():
-    print(midiParser.readNextData())
-'''
-print("---------------Testing MidiParser--------------")
-print("Header def and size: " + str(midiParser.readNextData()))
-headerBody = midiParser.readNextData()
-print("Body of header chunk: " + str(headerBody) + " number of tracks: " + 
-       str(int.from_bytes(headerBody[2:4], "big")))
-while midiParser.hasMoreData():
-#for i in range(2):
-    trackDef = midiParser.readNextData()
-    print("Track def and size: " + str(trackDef) + " track size: "
-        + str(int.from_bytes(trackDef[4:8], "big")))
-    while midiParser.bytesLeftInChunk > 0:
-        print(str(midiParser.readNextData()) + " size left: " +
-            str(midiParser.bytesLeftInChunk))
+def testEventDecoder():
+    print("-----Testing MidiEventDecoder---------")
+    #testing MidiEventDecoder
+    eventDecoder = MidiEventDecoder(midi_file) #testMidiFile.mid
+    print(eventDecoder.headerData())
+    #eventData = eventDecoder.nextEvent().midiData
+    #print(int.from_bytes(eventData[0:1],"big"))
+    #print(Util.msbIsOne(eventData))
+    #print(type(eventData))
+    while eventDecoder.hasMoreEvents():
+        event = eventDecoder.nextEvent()
+        print(event)
+    eventDecoder.close()
     print()
 
-'''
-trackDef = midiParser.readNextData()
-print("Track def and size: " + str(trackDef) + " track size: "
-    + str(int.from_bytes(trackDef[4:8], "big")))
-for i in range(65):
-    print(str(midiParser.readNextData()) + " size left: " +
-        str(midiParser.bytesLeftInChunk))
-'''
-midiParser.close()
-b = b'\x81\x7f'
-b = bytearray(b)
-print(Util.varLenVal(b))    
+def testMidiData():
+    print("-----Testing MidiData---------")
+    midiData = MidiData(midi_file)
+    for i in range(midiData.getNumTracks()):
+        track = midiData.getTrack(i)
+        print(track.name)
+        for note in track.notes:
+            print(note)
+        print()
+    print()
 
+#printRawFile()
+#testMidiParser()
+#testEventDecoder()
+testMidiData()
 
-print("\n\n\n-----Testing MidiEventDecoder---------")
-#testing MidiEventDecoder
-eventDecoder = MidiEventDecoder(midi_file) #testMidiFile.mid
-print(eventDecoder.headerData())
-#eventData = eventDecoder.nextEvent().midiData
-#print(int.from_bytes(eventData[0:1],"big"))
-#print(Util.msbIsOne(eventData))
-#print(type(eventData))
-while eventDecoder.hasMoreEvents():
-    print(eventDecoder.nextEvent())
-eventDecoder.close()
