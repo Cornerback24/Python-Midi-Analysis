fa98ee3a2088c5c1e08b25e95c90e0982e12a17c
Cornerback24 <6739979+Cornerback24@users.noreply.github.com>
2016-10-28 18:18:26 -0600

    style cleanup

--- MidiEventDecoder.py
+++ MidiEventDecoder.py
@@ -2,41 +2,47 @@ from MidiParser import MidiParser
 from MidiEvents import *
 from Util import Util
 
-#decodes data from the MidiParser into data easier to work with in MidiData
-#(will decode each piece of data from midiParser into an event,
-#including header chunk pieces)
+
+# decodes data from the MidiParser into data easier to work with in MidiData
+# (will decode each piece of data from midiParser into an event,
+# including header chunk pieces)
 class MidiEventDecoder:
     def __init__(self, midiFilename):
         self.midiParser = MidiParser(midiFilename)
         self.runningStatus = False
+        self.lastChannelStatusByte = None  # the first byte of the last channel event that didn't use running status
         return
+
     def hasMoreEvents(self):
         return self.midiParser.hasMoreData()
-    #be sure to call this once before calling nextEvent
+    # be sure to call this once before calling nextEvent
+
     def headerData(self):
         data = HeaderData()
         data.setFromBytes(self.midiParser.readNextData(),
                           self.midiParser.readNextData())
         return data
-    #returns a MidiEvent
+
+    # returns a MidiEvent
     def nextEvent(self):
         return self.midiEvent(self.midiParser.readNextData())
-    #creates a MidiEvent from the midiData
+    # creates a MidiEvent from the midiData
+
     def midiEvent(self, midiData):
-        #check if TrackHeader
+        # check if TrackHeader
         if midiData[0:4] == b'MTrk':
             trackHeader = TrackHeader()
             trackHeader.setFromBytes(midiData)
             return trackHeader
-        #find deltaTime
+        # find deltaTime
         tempData = midiData
-        temp = 0
-        while Util.msbIsOne(tempData[temp:]):
-            temp = temp+1
-        temp = temp + 1
-        deltaTime = tempData[:temp]
-        midiData = tempData[temp:]
-        #Meta Event
+        deltaTimeBytesLength = 0
+        while Util.msbIsOne(tempData[deltaTimeBytesLength:]):
+            deltaTimeBytesLength += 1
+        deltaTimeBytesLength += 1
+        deltaTime = tempData[:deltaTimeBytesLength]
+        midiData = tempData[deltaTimeBytesLength:]
+        # Meta Event
         if midiData[0:1] == b'\xff':
             if midiData[1] in EventDictionaries.META_EVENT_DICTIONARY:
                 metaEventClass = EventDictionaries.META_EVENT_DICTIONARY[midiData[1]]
@@ -46,19 +52,19 @@ class MidiEventDecoder:
             metaEvent.setDeltaTimeFromBytes(deltaTime)
             metaEvent.setFromBytes(midiData)
             return metaEvent
-        #System Event
+        # System Event
         if midiData[0:1] == b'\xf0' or midiData[0:1] == b'\xf7':
             systemEvent = SystemExclusiveEvent()
             systemEvent.setDeltaTimeFromBytes(deltaTime)
             systemEvent.setFromBytes(midiData)
             return systemEvent
-        #Channel Event
-        if Util.msbIsOne(midiData): #running status
+        # Channel Event
+        if Util.msbIsOne(midiData):  # running status
             self.lastChannelStatusByte = midiData[0]
-        else: #not running status
+        else:  # not running status
             midiData = self.lastChannelStatusByte + midiData
-        channelEventIdentifier = midiData[0] & int('f0',16)
-        if (channelEventIdentifier in EventDictionaries.CHANNEL_EVENT_DICTIONARY):
+        channelEventIdentifier = midiData[0] & int('f0', 16)
+        if channelEventIdentifier in EventDictionaries.CHANNEL_EVENT_DICTIONARY:
             channelEventClass = EventDictionaries.CHANNEL_EVENT_DICTIONARY[channelEventIdentifier]
         else:
             channelEventClass = ChannelEvent
@@ -66,6 +72,7 @@ class MidiEventDecoder:
         channelEvent.setDeltaTimeFromBytes(deltaTime)
         channelEvent.setFromBytes(midiData)
         return channelEvent
+
     def close(self):
         self.midiParser.close()
 
--- VisualOutputTesting.py
+++ VisualOutputTesting.py
@@ -2,17 +2,17 @@ from MidiParser import MidiParser
 from MidiData import MidiData
 from Util import Util
 from MidiEventDecoder import MidiEventDecoder
-from MidiEvents import *
 
 midi_file = "testMidiFile.mid"
-midi_file = "A Changeling Can Change_2.mid"
 print(midi_file)
+
 
 def printHex(_bytes):
     temp = ""
-    for i in range (len(_bytes)):
+    for i in range(len(_bytes)):
         temp = temp + " " + str(hex(_bytes[i]))
     print(temp)
+
 
 def printRawFile():
     print("-----------------raw file-----------------------")
@@ -21,39 +21,41 @@ def printRawFile():
     file.close()
     print()
     print()
-    
+
+
 def testMidiParser():
     midiParser = MidiParser(midi_file)
     print("---------------Testing MidiParser--------------")
     print("Header def and size: " + str(midiParser.readNextData()))
     headerBody = midiParser.readNextData()
-    print("Body of header chunk: " + str(headerBody) + " number of tracks: " + 
-           str(int.from_bytes(headerBody[2:4], "big")))
+    print("Body of header chunk: " + str(headerBody) + " number of tracks: " +
+          str(int.from_bytes(headerBody[2:4], "big")))
     while midiParser.hasMoreData():
-    #for i in range(2):
         trackDef = midiParser.readNextData()
         print("Track def and size: " + str(trackDef) + " track size: "
-            + str(int.from_bytes(trackDef[4:8], "big")))
+              + str(int.from_bytes(trackDef[4:8], "big")))
         while midiParser.bytesLeftInChunk > 0:
             print(str(midiParser.readNextData()) + " size left: " +
-                str(midiParser.bytesLeftInChunk))
+                  str(midiParser.bytesLeftInChunk))
         print()
     midiParser.close()
 
+
 def testEventDecoder():
     print("-----Testing MidiEventDecoder---------")
-    #testing MidiEventDecoder
-    eventDecoder = MidiEventDecoder(midi_file) #testMidiFile.mid
+    # testing MidiEventDecoder
+    eventDecoder = MidiEventDecoder(midi_file)  # testMidiFile.mid
     print(eventDecoder.headerData())
-    #eventData = eventDecoder.nextEvent().midiData
-    #print(int.from_bytes(eventData[0:1],"big"))
-    #print(Util.msbIsOne(eventData))
-    #print(type(eventData))
+    # eventData = eventDecoder.nextEvent().midiData
+    # print(int.from_bytes(eventData[0:1],"big"))
+    # print(Util.msbIsOne(eventData))
+    # print(type(eventData))
     while eventDecoder.hasMoreEvents():
         event = eventDecoder.nextEvent()
         print(event)
     eventDecoder.close()
     print()
+
 
 def testMidiData():
     print("-----Testing MidiData---------")
@@ -65,9 +67,9 @@ def testMidiData():
             print(note)
         print()
     print()
+    print("Note F4 329.04s to 339.32s Channel: 11 <-- expected last note (testMidiFile.mid)")
 
-#printRawFile()
-#testMidiParser()
-#testEventDecoder()
+# printRawFile()
+# testMidiParser()
+# testEventDecoder()
 testMidiData()
-
